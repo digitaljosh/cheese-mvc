2bda533fdf3cbff7527da8d83c5d182d946bdc38
Josh Markus <joshuajmarkus@gmail.com>
2017-08-13 23:44:52 -0700

    Refactored redundancies in 'delete item' functionality. Added 'name' validation and error message.

--- src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/com/launchcode/cheesemvc/controllers/CheeseController.java
@@ -22,44 +22,32 @@ public class CheeseController {
 
     static HashMap<String, String> cheeses = new HashMap<>();
 
-    static ArrayList<String> deletedCheeseList = new ArrayList<>();
+    static ArrayList<String> cheesesToDeleteList = new ArrayList<>();
 
     // request path will be /cheese due to @RequestMapping("cheese") above
-    @RequestMapping(value = "")  // path: /cheese
+    @RequestMapping(value = "")  // path /cheese
     public String index(Model model) {
 
-        if (deletedCheeseList.size() < 1) {
+        // if there are no items to delete
+        if (cheesesToDeleteList.size() < 1) {
             model.addAttribute("cheeses", cheeses);
             model.addAttribute("title", "My Cheeses");
 
+            // display list of cheeses
             return "cheese/index";
 
         // if there are items to be deleted:
         } else {
-            /** Originally had 'cheeses.remove(item)' in the 'if statement'  but is was
-             * throwing an error, I think because my cheese map was changing every iteration
-             * so I had to find a way to keep a list of items to remove from cheese hashmap
-             * without changing the hashmap while iterating
-             */
-            ArrayList<String> deleteList = new ArrayList<>();
-            for (String entry : cheeses.keySet()) {
-                if (deletedCheeseList.contains(entry)) {
-                    deleteList.add(entry); // adds entry to be deleted to deleteList
-                    }
-                }
-            /** iterates over each item in deleteList and
-             * deletes it from cheese HashMap
-              */
-            for (String item : deleteList) {
+            for (String item : cheesesToDeleteList) {
                 cheeses.remove(item);
             }
         }
-            deletedCheeseList.clear(); // clears deletedCheeseList so it doesn't affect subsequent deletions
+            cheesesToDeleteList.clear(); // clears deletedCheeseList so it doesn't affect subsequent deletions
             model.addAttribute("cheeses", cheeses);
             model.addAttribute("title", "My Cheeses");
 
+            // display amended list after deleting items
             return "cheese/index";
-
         }
 
 
@@ -73,11 +61,15 @@ public class CheeseController {
 
     // this handler processes the form
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription) {
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDescription, Model model) {
 
+        if (cheeseName.isEmpty()) {
+            model.addAttribute("title", "Add Cheese - 'Name' error!");
+            return "cheese/add";
+        }
         cheeses.put(cheeseName, cheeseDescription);
 
-        // redirect to /cheese, leaving it empty after ":" directs to parent path
+        // redirect to /cheese (leaving it empty after ":" directs to parent path)
         return "redirect:";
     }
 
@@ -96,7 +88,7 @@ public class CheeseController {
     public String processDeleteCheeseForm(@RequestParam ArrayList<String> deletedCheeses) {
 
         for (String item : deletedCheeses)
-            deletedCheeseList.add(item);
+            cheesesToDeleteList.add(item);
 
         return "redirect:";
     }
